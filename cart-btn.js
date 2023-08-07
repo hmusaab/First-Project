d745598b15dc7fc89c0dacea401dd77e81043aad
hmusaab <hafizmusaab8@gmail.com>
2023-08-07 05:19:56 -0800

    yes

---
+++ cart-btn.js
@@ -0,0 +1,24 @@
+const counter = 0;
+
+const addcardbtn = document.querySelectorAll('btn-for-card');
+//using foreach method
+// Add event listener to the "Add to Cart" buttons
+
+addcardbtn.forEach(button, () =>{
+    button.addEventListener('click', addcardbtn);
+    });
+
+
+
+
+    //function to add an item to the cart
+function addcardbtn(add){
+    
+
+
+};
+
+
+function clickcardbtn(){
+    
+}
